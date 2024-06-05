f7e7325106a055b65e40230aa9bb518dabd7c8e5
ParkSeongJun <sjyj98@naver.com>
2024-06-05 13:43:23 +0900

    6/5 Spring Security

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -4,22 +4,18 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.Size;
 
-
 //Database (Mysql)
 //Static List of todos => Database(h2, mysql)
-
-
 
 public class Todo {
 	private int id;
 	private String username;
-	
-	@Size(min=10, message="Enter at least 10 character")
+
+	@Size(min = 10, message = "Enter at least 10 character")
 	private String description;
 	private LocalDate targetDate;
 	private boolean done;
-	
-	
+
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
 		super();
 		this.id = id;
@@ -29,66 +25,50 @@ public class Todo {
 		this.done = done;
 	}
 
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getUsername() {
 		return username;
 	}
-
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-
 	public String getDescription() {
 		return description;
 	}
-
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
 	public LocalDate getTargetDate() {
 		return targetDate;
 	}
-
 
 	public void setTargetDate(LocalDate targetDate) {
 		this.targetDate = targetDate;
 	}
 
-
 	public boolean isDone() {
 		return done;
 	}
 
-
 	public void setDone(boolean done) {
 		this.done = done;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Todo [id=" + id + ", username=" + username + ", description=" + description + ", targetDate="
 				+ targetDate + ", done=" + done + "]";
 	}
-	
-	
-	
-	
-	
 
 }
--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -7,29 +7,31 @@ import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.Valid;
 
 @Service
 public class TodoService {
 
 	private static List<Todo> todos = new ArrayList<>();
-	
+
 	private static int todosCount = 0;
-	
+
 	static {
-		todos.add(new Todo(++todosCount, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "in28minutes", "GET AWS Certified", LocalDate.now().plusYears(1), false));
 		todos.add(new Todo(++todosCount, "in28minutes", "Learn DevOps", LocalDate.now().plusYears(2), false));
 		todos.add(new Todo(++todosCount, "in28minutes", "Learn Full Stack dev", LocalDate.now().plusYears(3), false));
 	}
-	
+
 	public List<Todo> findByUsername(String username) {
-		return todos;
+		Predicate<? super Todo> predicate = todo -> todo.getUsername().equals(username);
+		return todos.stream().filter(predicate).toList();
 	}
-	
+
 	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
-		Todo todo = new Todo(++todosCount, username,description,targetDate,done);
+		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
 		todos.add(todo);
 	}
-	
+
 	public void deleteById(int id) {
 		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
 		todos.removeIf(predicate);
@@ -40,6 +42,11 @@ public class TodoService {
 		Todo todo = todos.stream().filter(predicate).findFirst().get();
 		return todo;
 	}
-	
-	
+
+	public void updateTodo(@Valid Todo todo) {
+		deleteById(todo.getId());
+		todos.add(todo);
+
+	}
+
 }
